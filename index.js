e55dc2880bb60587279a9255e3d6f3a3c3d1b89c
enhbayasgalan <enhuush0109@gmail.com>
2024-12-16 18:28:29 +0800

    dom

---
+++ index.js
@@ -0,0 +1,34 @@
+// const parent = document.getElementById("game-container").innerHTML = "hello world";
+// const parent2 = document.getElementById("game-container").style()
+
+// const p_tag = document.createElement("p");
+// p_tag.innerHTML = "i need p tag";
+
+// document.getElementById("game-container").appendChild(p_tag);
+
+// const new_element = document.createElement("div");
+// new_element.innerHTML = "replead";
+
+// document.getElementById("game-container").replaceChild(new_element, p_tag);
+
+// new_element.style.color = "red";
+// new_element.style.fontSize = "60px";
+
+
+
+// new_element.addEventListener("mouseenter", () => {
+//     new_element.style.color = "red";
+// });
+
+// new_element.addEventListener("mouseout", () => {
+//     new_element.style.color = "green";
+// });
+
+// new_element.addEventListener("click", () => {
+//     console.log("clicked");
+// });
+
+// const butt
+
+
+
